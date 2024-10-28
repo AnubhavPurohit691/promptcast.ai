fb95341a048bb283b74013f18c9677d1ba0cafb3
Anubhav purohit <anubhavpurohitno1@gmail.com>
2024-10-28 20:42:21 +0530

    commit4

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -3,7 +3,7 @@ import { Vortex } from "./ui/vortex"
 
 const Body = () => {
   return (
-    <div className="lg:my-72  lg:px-40">
+    <div className="lg:my-72 my-32 px-4 lg:px-40">
       <Vortex>
 
       <TextGenerateEffectDemo />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 
 const Navbar = () => {
   return (
-    <div className="flex  justify-between items-center text-white px-4 py-6 md:px-10 lg:px-20">
+    <div className="flex  justify-between items-center text-white px-4 py-3 lg:py-6 md:px-10 lg:px-20">
       <div className="text-2xl md:text-3xl">Promtcast.AI</div>
       <button className="bg-white text-black font-bold py-2 px-4 rounded-full mt-2 md:mt-0 text-sm md:text-base">
         Login
